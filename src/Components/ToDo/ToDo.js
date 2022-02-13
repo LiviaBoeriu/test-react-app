3d312e6e5b73ad0d9f7d74ce3250fbb657e958e3
Livia Boeriu <boeriulivia@gmail.com>
2022-02-13 22:22:58 +0100

    Minor name fix for checkbox

--- src/Components/ToDo/ToDo.js
+++ src/Components/ToDo/ToDo.js
@@ -8,7 +8,7 @@ function ToDo(props) {
         <li className={`todo ${props.data.isDone ? 'is-done' : ''}`}>
             <button className='todo-button' onClick={() => { props.deleteTodo(props.data.id) }}><DeleteIcon /></button>
             <label className='todo-text' htmlFor={props.data.id}>{props.data.text}</label>
-            <input type="checkbox" id={props.data.id} name="topping" value="value" onChange={() => { props.completeToDo(props.data.id) }} />
+            <input type="checkbox" id={props.data.id} name="todo-checkbox" value="value" onChange={() => { props.completeToDo(props.data.id) }} />
         </li>
     )
 }
