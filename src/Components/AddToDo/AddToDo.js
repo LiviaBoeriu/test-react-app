ba3a3333da275dc27614e492e96c2ed2721ecc85
Livia Boeriu <boeriulivia@gmail.com>
2022-02-16 22:48:33 +0100

    Added input feedback message

--- src/Components/AddToDo/AddToDo.js
+++ src/Components/AddToDo/AddToDo.js
@@ -8,10 +8,11 @@ class AddToDo extends React.Component {
         super(props);
         this.state = {
             value: '',
+            isError: false
         }
         // This binding is necessary to make `this` work in the callback
         this.handleClick = this.handleClick.bind(this);
-        this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.handleKeyUp = this.handleKeyUp.bind(this);
     }
 
     // On change assign the input value to the state
@@ -21,20 +22,30 @@ class AddToDo extends React.Component {
 
     // Function to add a new to do to the list
     handleClick() {
-        if (!this.state.value.length) {
-            console.log('Upps you must have text')
+        if (!this.state.value) {
+            this.setState({ isError: true })
         } else {
             this.props.addToDo(this.state.value);
             this.setState({ value: '' });
         }
     }
 
-    // On enter add a new to do to the list
-    // Missing handling in case input is empty
-    handleKeyPress(e) {
-        if (e.charCode === 13) {
-            this.props.addToDo(this.state.value);
-            this.setState({ value: '' });
+    handleKeyUp(e) {
+        if (e.keyCode !== 13) {
+            if (e.target.value) {
+                this.setState({ isError: false })
+            } else {
+                this.setState({ isError: true })
+            }
+        } else {
+            if (!e.target.value) {
+                this.setState({ isError: true })
+            } else {
+                this.props.addToDo(this.state.value);
+                this.setState({
+                    value: ''
+                });
+            }
         }
     }
 
@@ -43,9 +54,18 @@ class AddToDo extends React.Component {
             <div className='add-box'>
                 <CurrentDate />
                 <div className="enter-box">
-                    <input type="text" id="addToDo" name="addToDo" placeholder='Add a new to do' value={this.state.value} onChange={this.handleChange} onKeyPress={this.handleKeyPress} />
-                    <Button className="add-button" handleClick={this.handleClick} />
+                    <input
+                        type="text"
+                        id="addToDo"
+                        name="addToDo"
+                        placeholder='Add a new to do'
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        onKeyUp={this.handleKeyUp}
+                    />
+                    <Button className='add-button' handleClick={this.handleClick} />
                 </div>
+                <p className={`feedback ${this.state.isError ? 'show' : ''}`}>Field is empty</p>
             </div>
         )
     }
